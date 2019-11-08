eccf708e8e8fa3d7a77aa31a33197686a88f4509
Stadionova <726383@mail.ru>
2019-11-08 14:58:17 +0300

    created grid blocks for whole page

---
+++ onederx-react/src/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './App.css';
+// import Chart from "../Chart/Chart";
+// import Header from "../Header/Header";
+// import ChartPeriods from '../ChartPeriods/ChartPeriods';
+// import { getDataComeYear, getDataBornYear, getDataDieYear, getDataLeaveYear } from '../../getFunctionPeriod';
+// import { connect } from 'react-redux';
+
+class App extends React.Component {
+
+  // state = {
+
+  // }
+
+  render() {
+    return (
+      <div className='containerApp'>
+        <div className='container-grid'>
+          <div class="containerApp__account">
+            <div className="chart-1">onederx</div>
+            <div className="chart-2"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+// export default connect(
+//   state => ({
+
+//   })
+// )(App);
+
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
