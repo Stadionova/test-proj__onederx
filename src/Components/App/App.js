f392f2bff9a04d9f93bf7e661548d33b5a411ea1
Stadionova <726383@mail.ru>
2019-11-09 16:39:39 +0300

    added user info in Chart2

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -13,25 +13,30 @@ class App extends React.Component {
             <div className="chart-2">
               <div className='account-settings'>
                 <div><h3>Account settings</h3></div>
-                <div className='User-info'>
-                  <div>
-                    <div><span>User info</span></div>
-                    <div><button>Change</button></div>
+                <div className='user-blocks'>
+                  <div className='user-info'>
+                    <div className="user-info-change">
+                      <h3>User info</h3>
+                      <div><button>Change</button></div>
+                    </div>
+                    <div className='user-info-name'>
+                      <div className='grey'><span>Name</span></div>
+                      <div className='white'><span>Alex Salio</span></div>
+                    </div>
+                    <div className='user-info-country'>
+                      <div className='grey'><span>Country of Residence</span></div>
+                      <div className='white'><span>Ukraine</span><span>UA</span></div>
+                    </div>
                   </div>
-                  <div>
-                    <div><span>Name</span></div>
-                    <div><span>Alex Salio</span></div>
-                  </div>
-                  <div>
-                    <div><span>Country of Residence</span></div>
-                    <div><span>Ukraine UA</span></div>
-                  </div>
-                </div>
-                <div className='Log-in'>
-                  <div>
-                    <div><span>Password</span></div>
-                    <div><input value="*********"></input></div>
-                    <div><button>Change</button></div>
+                  <div className='log-in'>
+                    <div><h3>Log in</h3></div>
+                    <div>
+                      <div className='grey'><span>Password</span></div>
+                      <div className='value'>
+                        <div><input value="*********"></input></div>
+                        <div><button>Change</button></div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
