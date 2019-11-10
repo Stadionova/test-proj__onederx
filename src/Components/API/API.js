bbd5636bf3a015818450113f3cdb7f963710708a
Stadionova <726383@mail.ru>
2019-11-10 16:28:10 +0300

    aligned header blocks

--- src/Components/API/API.js
+++ src/Components/API/API.js
@@ -5,8 +5,8 @@ const API = () => {
 
   return (
     <div className='API'>
-      <div className='API__buttons'><span>Trade</span></div>
-      <div className='API__buttons'><span>Documentation</span></div>
+      <div className='API__buttons first'><span>Trade</span></div>
+      <div className='API__buttons Documentation'><span>Documentation</span></div>
       <div className='API__buttons'><span>API</span></div>
     </div>
   )
