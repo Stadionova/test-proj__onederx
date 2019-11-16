62b5ec7e3c45906477432234307b180b6175b863
Stadionova <726383@mail.ru>
2019-11-16 13:31:15 +0300

    changed classes names in Login.js

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -9,33 +9,29 @@ class LogIn extends React.Component {
     return this.props.loginInEditMode ?
       <ModalWindowLogIn />
       : (
-        <div className='log-in'>
+        <div className='logIn__container'>
           <div><h3>Log in</h3></div>
-          <div className='inputAndButton'>
-            <div className='grey'><span>Password</span></div>
-            <div className='value'>
-              <div><span>{lengthToStars(this.props.password.length)}</span></div>
-              <div className='change-button'><button className='buttonChange' onClick={this.openModalWindowLogIn.bind(this)}>Change</button></div>
+          <div className='logIn__name'>
+            <div><span>Password</span></div>
+            <div className='logIn__value'>
+              <div><span>{this.changeLengthToStars(this.props.password.length)}</span></div>
+              <div className='logIn__change'>
+                <button onClick={this.openModalWindowLogIn}>Change</button>
+              </div>
             </div>
           </div>
         </div>
       );
   }
 
-  typePassword(e) {
-    if (e.key === 'Enter') {
-      const password = e.currentTarget.value;
-      this.props.savePassword(password);
-    }
+  openModalWindowLogIn = () => {
+    this.props.showEditModeLogIn();
   }
 
-  openModalWindowLogIn() {
-    this.props.showModalLogIn();
+  changeLengthToStars = (length) => {
+    return "•".repeat(length);
   }
-}
 
-function lengthToStars(length) {
-  return "•".repeat(length);
 }
 
 export default connect(
@@ -44,7 +40,6 @@ export default connect(
     loginInEditMode: state.loginInEditMode
   }),
   dispatch => ({
-    savePassword: (password) => dispatch({ type: "DATA_PASSWORD", payload: password }),
-    showModalLogIn: () => dispatch({ type: "CHANGE_WINDOW_LOGIN_STATUS_TRUE" })
+    showEditModeLogIn: () => dispatch({ type: "CHANGE_WINDOW_LOGIN_STATUS_TRUE" })
   })
 )(LogIn);
